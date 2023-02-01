3b7c7d715bb34720177be8c23218a47c529b32a0
Stent <stentonian@protonmail.com>
2023-02-01 13:04:16 +0000

    Update verify script to take multiple rules

--- certora/scripts/verifySyndicate.sh
+++ certora/scripts/verifySyndicate.sh
@@ -2,10 +2,10 @@
 # where $1 is a comment for the run that gives some background to the call and $2 is an 
 # optional arguement used to specify a rule, without a specified rule all rules in the 
 # spec will be ran.
-if [[ "$2" ]]
-then
-    RULE="--rule $2"
-fi
+# if [[ "$2" ]]
+# then
+#     RULE="--rule $2"
+# fi
 
 solc-select use 0.8.13
 
@@ -19,8 +19,8 @@ certoraRun  certora/harnesses/SyndicateHarness.sol \
     --optimistic_loop \
     --optimize 1 \
     --loop_iter 3 \
-    $RULE \
+    $2 \
     --rule_sanity \
     --settings -optimisticFallback=true \
     --packages @blockswaplab=node_modules/@blockswaplab @openzeppelin=node_modules/@openzeppelin \
-    --msg "Syndicate $1 $2"
+    --msg "Syndicate $1"
